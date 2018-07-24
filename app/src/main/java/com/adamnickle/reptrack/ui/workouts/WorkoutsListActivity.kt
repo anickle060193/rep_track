94d0495d03e7c56a1644671d37dcc51f4c20d9be
Adam Nickle <anickle060193@gmail.com>
2018-07-23 23:40:59 -0500

    Added ability to set Workout names.

--- app/src/main/java/com/adamnickle/reptrack/ui/workouts/WorkoutsListActivity.kt
+++ app/src/main/java/com/adamnickle/reptrack/ui/workouts/WorkoutsListActivity.kt
@@ -4,12 +4,15 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.RecyclerView
 import android.view.WindowManager
 import com.adamnickle.reptrack.AppExecutors
 import com.adamnickle.reptrack.BuildConfig
 import com.adamnickle.reptrack.R
 import com.adamnickle.reptrack.databinding.ActivityWorkoutsListBinding
+import com.adamnickle.reptrack.databinding.InputDialogBinding
 import com.adamnickle.reptrack.model.workout.Workout
 import com.adamnickle.reptrack.model.workout.WorkoutDao
 import com.adamnickle.reptrack.ui.ViewModelFactory
@@ -35,6 +38,8 @@ class WorkoutsListActivity: DaggerAppCompatActivity()
 
     private lateinit var viewModel: WorkoutsListViewModel
 
+    private var inputDialog: AlertDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -47,23 +52,65 @@ class WorkoutsListActivity: DaggerAppCompatActivity()
             adapter.submitList( result?.sortedByDescending { workout -> workout.date } )
         } )
 
-        this.adapter = WorkoutsListAdapter( appExecutors ) {
+        adapter = WorkoutsListAdapter( appExecutors ) {
             workout -> println( "Workout clicked: $workout")
         }
+
+        adapter.registerAdapterDataObserver( object: RecyclerView.AdapterDataObserver()
+        {
+            override fun onItemRangeInserted( positionStart: Int, itemCount: Int )
+            {
+                binding.workoutsList.smoothScrollToPosition( positionStart )
+            }
+        } )
 
         binding.workoutsList.adapter = adapter
         binding.workoutsList.addItemDecoration( DividerItemDecoration( this, DividerItemDecoration.VERTICAL ) )
 
         binding.workoutAdd.setOnClickListener {
-            appExecutors.diskIO().execute {
-                workoutDao.insertWorkout( Workout( "Testing - ${( Math.random() * 10 ).toInt()}", Date() ) )
-                binding.workoutsList.smoothScrollToPosition( 0 )
-            }
+
+            val inputBinding = DataBindingUtil.inflate<InputDialogBinding>( layoutInflater, R.layout.input_dialog, null, false )
+            inputBinding.defaultValue = getString( R.string.shortDateFormat, Date() )
+
+            inputDialog = AlertDialog.Builder( this )
+                    .setTitle( "Workout Name" )
+                    .setView( inputBinding.root )
+                    .setPositiveButton( android.R.string.ok, null )
+                    .setNegativeButton( android.R.string.cancel, null )
+                    .create()
+                    .also { dialog ->
+                        dialog.window.setSoftInputMode( WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE )
+                        dialog.setOnShowListener {
+                            dialog.getButton( AlertDialog.BUTTON_POSITIVE ).setOnClickListener {
+                                val workoutName = inputBinding.input.text.toString()
+                                if( !workoutName.isBlank() )
+                                {
+                                    appExecutors.diskIO().execute {
+                                        workoutDao.insertWorkout( Workout( workoutName, Date() ) )
+                                    }
+
+                                    dialog.dismiss()
+                                }
+                                else
+                                {
+                                    inputBinding.input.error = "Cannot be empty"
+                                }
+                            }
+                        }
+                    }
+            inputDialog?.show()
         }
 
         if( BuildConfig.DEBUG )
         {
             window.addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON )
         }
+    }
+
+    override fun onDestroy()
+    {
+        inputDialog?.dismiss()
+
+        super.onDestroy()
     }
 }
